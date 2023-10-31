ebf4b98295200fa39f0cdb9b4d31909e600347a0
Ragnar <149425716+Ragnar-X1@users.noreply.github.com>
2023-10-31 16:52:30 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // This treat all JS code as newer version / It is not compulsory
+
+// alert (4+4) // This will not work beacouse we are using nodejs, not browser
+  
+// Visit "Echma Scripts" OR "mdn" for the documentation of JavaScript
+
+console.log("Ragnar")
+
+let name = "Ragnar" //variable
+let age = 20  //integer
+let isLoogedIn = false //boolean
+
+
+// Primitive DataTypes
+// number -> 2 to the power of 53
+// bigint
+// string -> "  "
+// boolean -> true/false
+// null -> standalone value / It is also identified as object
+// undefined -> values is not assigned
+// symbol -> for representing unique
+
+// Objects
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
