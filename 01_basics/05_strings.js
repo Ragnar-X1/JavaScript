656d83d36ac87b3eef0178113c16b78a3c0a7f45
Ragnar <149425716+Ragnar-X1@users.noreply.github.com>
2023-11-01 10:43:17 +0000

    String Discussed

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+let name = "Ragnar"
+const num = 100;
+
+console.log(typeof(name))
+
+// console.log(name + num)  // Old style
+
+console.log(`My name is ${name} and  I have 100 accounts`);
+
+
+// Declaring a string in another way
+
+const getName = new String("Ragnar Lothbrook");
+console.log(getName) 
+console.log(typeof(getName))
