6359aa06663b5e5caf963c101809bee6e78b4238
Ragnar <149425716+Ragnar-X1@users.noreply.github.com>
2023-12-06 15:55:04 +0000

    Object destructuring

---
+++ practice/03_ObjectDestructuring.js
@@ -0,0 +1,21 @@
+// Object Destructuring
+
+
+const course = {
+    coursename: "Vikings",
+    price: "Blood",
+    courseInstructor: "Ragnar"
+}
+
+console.log(course)
+
+const {courseInstructor: teacher} = course
+
+console.log(teacher)
+
+
+
+
+
+
+
