fc5eb2c34a357a41ab2a535843cb8cd684b5c186
Ragnar <149425716+Ragnar-X1@users.noreply.github.com>
2023-12-07 07:07:32 +0000

    This and Arrow Function

---
+++ practice/06_This&Arrow.js
@@ -0,0 +1,42 @@
+// // This Function 
+
+
+// const user = {
+//     name: "Ragnar",
+//     userId: "God",
+//     welcomeMessage: function(){
+//         console.log(`Hello ${this.name}, Welcome to Valhalla`)
+//     }
+// }
+
+// console.log(user)
+
+// // console.log(user.welcomeMessage())
+// user.welcomeMessage()
+
+
+
+// Arrow Function 
+
+// const welcome = function(){
+//     const name = "Ragnar"
+//     console.log(`Welcome ${this.name}, This is Valhalla`)
+// }
+
+// welcome()
+
+// const welcome = () =>  {
+//     let name = "Ragnar"
+//     console.log(`Hello ${this.name}, Welcome to Valhalla`)
+// }
+
+// welcome()
+
+
+// const add = (a,b) => {
+//     return a+b
+// }
+
+const add = (a,b) => a+b 
+
+console.log(add(2,4))
